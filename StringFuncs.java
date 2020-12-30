4f15ee789e8df1737659784eb7ed88c0725fcfe6
Sudarshan Kannan <sudarshan.kannan99@gmail.com>
2020-12-30 15:45:28 -0800

    hello

--- StringFuncs.java
+++ StringFuncs.java
@@ -3,9 +3,18 @@ import java.util.HashMap;
 class StringFuncs{
     //isUnique
     boolean isUnique(String str){
-        HashMap
-        return false;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for(int i=0; i<str.length(); i++){
+            if(map.containsKey(str.substring(i,i+1))==false){
+                map.put(str.substring(i,i+1), 1);
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
     }
     //checkPermutation
+    boolean checkPermutation()
     //URLify
 }
